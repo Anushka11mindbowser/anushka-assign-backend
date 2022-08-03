6355bbe86c84a133c3e680d2c6ae312c4443b09c
Anushka11mindbowser <100129927+Anushka11mindbowser@users.noreply.github.com>
2022-08-03 13:47:31 +0530

    StripeEventListener View updated

--- cardUser/views.py
+++ cardUser/views.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+import json
 from django.shortcuts import redirect
 import stripe
 from django.views.decorators.csrf import csrf_exempt
@@ -73,6 +74,7 @@ class StripeWebhookView(CreateAPIView):
             },
             'unit_amount': 2000,
         },
+        
         'quantity': 1,
         }],
         mode='payment',
@@ -82,14 +84,38 @@ class StripeWebhookView(CreateAPIView):
 
         return Response("Checkout Completed")
        
+       
 
        
     
 
 class StripeEventListener(CreateAPIView):
     def post(self, request, *args, **kwargs):
-        print(request.body)
-        return Response(request.body)
+        payload = request.body
+        event = None
+
+        try:
+            event = stripe.Event.construct_from(
+            json.loads(payload), stripe.api_key
+            )
+        except ValueError as e:
+        # Invalid payload
+            return Response(status=400)
+
+    # Handle the event
+        if event.type == 'payment_intent.succeeded':
+            payment_intent = event.data.object # contains a stripe.PaymentIntent
+        # Then define and call a method to handle the successful payment intent.
+        # handle_payment_intent_succeeded(payment_intent)
+        elif event.type == 'payment_method.attached':
+            payment_method = event.data.object # contains a stripe.PaymentMethod
+        # Then define and call a method to handle the successful attachment of a PaymentMethod.
+        # handle_payment_method_attached(payment_method)
+    # ... handle other event types
+        else:
+            print('Unhandled event type {}'.format(event.type))
+
+        return Response(status=200)
     
     
 
